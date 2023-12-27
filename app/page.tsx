017b0eddf7d03d328c865c8663e2b7dfcc275b1a
H-Tsukasa <mip68556@gmail.com>
2023-12-28 04:42:41 +0900

    refactor: ♻️ githubactions のテスト

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import FetchItem from "@/components/FetchItem";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import FetchItem from '@/components/FetchItem';
 
 const Home: NextPage = () => {
     return (
