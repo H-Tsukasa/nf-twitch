b607acb4009a3b89088d893c32a396285c744023
H-Tsukasa <mip68556@gmail.com>
2024-01-13 00:23:36 +0900

    fix: 🐛 サーバの修正

--- api/config.py
+++ api/config.py
@@ -24,7 +24,7 @@ PASSWD = os.environ.get('DB_PASSWORD'),
 HOST = os.environ.get('DB_HOST'),
 DB = os.environ.get('DB_NAME')
 
-connection_string = f"mysql+mysqlconnector://{USER[0]}:{PASSWD[0]}@{HOST[0]}:3306/{DB}"
+connection_string = f"mysql://{USER[0]}:{PASSWD[0]}@{HOST[0]}:3306/{DB}"
 ENGINE = create_engine(connection_string, echo=True)
 
 session = scoped_session(sessionmaker(autoflush=False, bind=ENGINE))
